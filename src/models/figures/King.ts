7c5fc6c28b39726fb6759ac3140b1c5bb2364fac
andrii-ostapov <167943AOR>
2022-07-07 23:19:40 +0300

    Create figures

---
+++ src/models/figures/King.ts
@@ -0,0 +1,13 @@
+import {Figure, FigureNames} from "./Figure";
+import {Colors} from "../Colors";
+import {Cell} from "../Cell";
+import blackLogo from "../../assets/Chess_kdt60.png";
+import whiteLogo from "../../assets/Chess_klt60.png";
+
+export class King extends Figure {
+    constructor(color: Colors, cell: Cell) {
+        super(color, cell);
+        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+        this.name = FigureNames.KING
+    }
+}
